c1b249a66fff2059b7449adb7e225da595c78c53
dillonpw <dillonpwalsh10@gmail.com>
2023-12-19 23:50:52 -0500

    attempting to correct deployment issues

--- project/astro.config.mjs
+++ project/astro.config.mjs
@@ -1,9 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import vercelStatic from "@astrojs/vercel/static";
 
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel()
+  output: "static",
+  adapter: vercelStatic(),
 });
