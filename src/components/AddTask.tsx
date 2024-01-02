4fa8005a72d3b5bf66627d3c7851d1a63b1a80bc
dillonpw <dillonpwalsh10@gmail.com>
2024-01-01 21:22:44 -0500

    bug fixing

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,28 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 interface AddTaskProps {
   onAddTask: (taskText: string) => void;
 }
 
 const AddTask: React.FC<AddTaskProps> = ({ onAddTask }) => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     onAddTask(input);
-    setInput('');
+    setInput("");
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="taskInput">Task:</label>
+      <label htmlFor="taskInput"></label>
       <input
         id="taskInput"
         type="text"
         value={input}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInput(e.target.value)}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setInput(e.target.value)
+        }
         placeholder="What else?"
       />
+
       <button type="submit">Add Task</button>
     </form>
   );
