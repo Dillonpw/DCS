f56593adbe04bc6a46eb4cd29e54f2689e2ccf4d
dillonpw <dillonpwalsh10@gmail.com>
2024-01-18 16:13:11 -0500

    updated modal location and content

--- src/components/TodoComp.tsx
+++ src/components/TodoComp.tsx
@@ -13,7 +13,10 @@ const Todo: React.FC = () => {
   const [tasks, setTasks] = useState<Task[]>(getInitialTasks());
 
   useEffect(() => {
-    Cookies.set("tasks", JSON.stringify(tasks), { expires: 100, sameSite: 'Lax' });
+    Cookies.set("tasks", JSON.stringify(tasks), {
+      expires: 100,
+      sameSite: "Lax",
+    });
   }, [tasks]);
 
   function handleAddTask(text: string) {
@@ -37,12 +40,12 @@ const Todo: React.FC = () => {
 
   return (
     <div className="container todo">
-        <AddTask onAddTask={handleAddTask} />
-        <TaskList
-          tasks={tasks}
-          onChangeTask={handleChangeTask}
-          onDeleteTask={handleDeleteTask}
-        />
+      <AddTask onAddTask={handleAddTask} />
+      <TaskList
+        tasks={tasks}
+        onChangeTask={handleChangeTask}
+        onDeleteTask={handleDeleteTask}
+      />
     </div>
   );
 };
@@ -51,11 +54,13 @@ let nextId = getInitialMaxId() + 1;
 
 function getInitialTasks(): Task[] {
   const savedTasks = Cookies.get("tasks");
-  const tasks = savedTasks ? JSON.parse(savedTasks) : [
-    { id: 0, text: "Log into systems", done: false },
-    { id: 1, text: "Check the schedule", done: false },
-    { id: 2, text: "Test radios and tones", done: false },
-  ];
+  const tasks = savedTasks
+    ? JSON.parse(savedTasks)
+    : [
+        { id: 0, text: "Log into systems", done: false },
+        { id: 1, text: "Check the schedule", done: false },
+        { id: 2, text: "Test radios and tones", done: false },
+      ];
   return tasks;
 }
 
